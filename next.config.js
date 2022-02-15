13827265228731492cb2ca9591ae1d196125e238
Dante Calderon <mail@dantecalderon.dev>
2022-02-15 00:09:32 -0500

    Disable trailing slash

--- next.config.js
+++ next.config.js
@@ -1,2 +1,5 @@
-const withImages = require('next-images')
-module.exports = withImages()
+const withImages = require('next-images');
+module.exports = {
+  ...withImages(),
+  trailingSlash: false,
+};
