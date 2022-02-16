bad6e70e4c2f5b9de9e910d1669d5b46260a820c
Dante Calderon <mail@dantecalderon.dev>
2022-02-15 22:05:43 -0500

    Fix broken

--- logger/pino.logger.ts
+++ logger/pino.logger.ts
@@ -5,24 +5,24 @@ import { logflarePinoVercel } from 'pino-logflare'
 // Browser logs are going to: https://logflare.app/sources/13989
 // Vercel log drain was setup to send logs here: https://logflare.app/sources/18640
 
-const { stream, send } = logflarePinoVercel({
-    apiKey: process.env.LOGFARE_API_KEY,
-    sourceToken: process.env.LOGFARE_SOURCE_TOKEN
-});
+// const { stream, send } = logflarePinoVercel({
+//     apiKey: process.env.LOGFARE_API_KEY,
+//     sourceToken: process.env.LOGFARE_SOURCE_TOKEN
+// });
 
 // create pino logger
-const logger = pino({
-    browser: {
-        transmit: {
-            level: "info",
-            send: send,
-        }
-    },
-    level: "debug",
-    base: {
-        env: process.env.NODE_ENV,
-        revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
-    },
-}, stream);
+// const logger = pino({
+//     browser: {
+//         transmit: {
+//             level: "info",
+//             // send: send,
+//         }
+//     },
+//     level: "debug",
+//     base: {
+//         env: process.env.NODE_ENV,
+//         revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
+//     },
+// });
 
-export default logger
+export default console
